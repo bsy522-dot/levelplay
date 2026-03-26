225633ae5e15eb84114f9b2747603e1f1de2d4b6
user <user@example.com>
2026-03-26 11:16:54 +0900

    Kill service worker: unregister all SW + delete all caches on page load

--- sw.js
+++ sw.js
@@ -1,34 +1,9 @@
-const CACHE_NAME = 'levelplay-v4';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './games/hatcuping-game.html',
-  './games/hatcuping-rpg.html'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
+// 서비스 워커 자체 해제 - 모든 캐시 삭제하고 등록 해제
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(names =>
-      Promise.all(names.map(n => caches.delete(n)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Network-first: 항상 최신 버전 먼저 시도
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-      return response;
-    }).catch(() => caches.match(event.request))
+    caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
